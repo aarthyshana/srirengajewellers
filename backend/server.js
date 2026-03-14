fe50ce1937427c5143d01f76343a5df303827353
Aarthy R <aarthyshana@gmail.com>
2026-03-14 18:17:27 +0530

    fixed render port

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.port || 3000;
 
 // Middleware
 app.use(cors());
